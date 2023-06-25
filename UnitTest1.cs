c9185e86e5e0f0bf902baa4740e06c62859b9b0e
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-06-25 20:27:56 +0530

    [Manish Thakur]: Added 2nd program

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -2,69 +2,39 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace PurchaseAnalyzerTest
 {
-    [TestClass]
-    public class PurchaseAnalyzerTests
+public class GroupEligibilityCheckerTests
+{
+    [TestMethod]
+    public void IsGroupEligible_WithEligibleWords_ReturnsTrue()
     {
-        [TestMethod]
-        public void GetPriceOfItemSoldOnce_WithValidInput_ReturnsCorrectPrice()
-        {
-            // Arrange
-            string purchases = "5 5 4 7 4 1 11";
-            int expectedPrice = 7;
-            var analyzer = new PurchaseAnalyzer();
+        // Arrange
+        string word1 = "noon";
+        string word2 = "moon";
+        bool expected = false;
+        var checker = new GroupEligibilityChecker();
 
-            // Act
-            int actualPrice = analyzer.GetPriceOfItemSoldOnce(purchases);
+        // Act
+        bool actual = checker.IsGroupEligible(word1, word2);
 
-            // Assert
-            Assert.AreEqual(expectedPrice, actualPrice);
-        }
-
-        [TestMethod]
-        public void GetPriceOfItemSoldOnce_WithNoUniquePurchases_ReturnsNone()
-        {
-            // Arrange
-            string purchases = "5 5 5 5";
-            int expectedPrice = 0; // Assuming 0 indicates "none"
-            var analyzer = new PurchaseAnalyzer();
-
-            // Act
-            int actualPrice = analyzer.GetPriceOfItemSoldOnce(purchases);
-
-            // Assert
-            Assert.AreEqual(expectedPrice, actualPrice);
-        }
-
-        [TestMethod]
-        public void GetPriceOfItemSoldMoreThanOnce_WithValidInput_ReturnsCorrectPrice()
-        {
-            // Arrange
-            string purchases = "5 5 4 7 4 1 11";
-            int expectedPrice = 5;
-            var analyzer = new PurchaseAnalyzer();
-
-            // Act
-            int actualPrice = analyzer.GetPriceOfItemSoldMoreThanOnce(purchases);
-
-            // Assert
-            Assert.AreEqual(expectedPrice, actualPrice);
-        }
-
-        [TestMethod]
-        public void GetPriceOfItemSoldMoreThanOnce_WithNoRepeatedPurchases_ReturnsNone()
-        {
-            // Arrange
-            string purchases = "1 2 3 4 5";
-            int expectedPrice = 0; // Assuming 0 indicates "none"
-            var analyzer = new PurchaseAnalyzer();
-
-            // Act
-            int actualPrice = analyzer.GetPriceOfItemSoldMoreThanOnce(purchases);
-
-            // Assert
-            Assert.AreEqual(expectedPrice, actualPrice);
-        }
+        // Assert
+        Assert.AreEqual(expected, actual);
     }
 
+    [TestMethod]
+    public void IsGroupEligible_WithIneligibleWords_ReturnsFalse()
+    {
+        // Arrange
+        string word1 = "part";
+        string word2 = "trap";
+        bool expected = true;
+        var checker = new GroupEligibilityChecker();
+
+        // Act
+        bool actual = checker.IsGroupEligible(word1, word2);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+}
 }
 
